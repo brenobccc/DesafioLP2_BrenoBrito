251c62df4647e53a7e298d43cc829a616722af33
brenobccc <brenobritobc@gmail.com>
2021-09-17 14:16:02 -0300

    inicializando projeto

---
+++ src/main/java/br/edu/ifce/lp2/SistemaClientesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifce/lp2/SistemaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
